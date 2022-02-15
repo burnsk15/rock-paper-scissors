f0f0fe009c96451a1660618296f53f291ed9564e
Kathleen <burnsk15@yahoo.com>
2022-02-15 12:54:08 -0500

    add function to ask and store users choice

--- myScript.js
+++ myScript.js
@@ -4,14 +4,27 @@ function computerPlay() {
     let num = Math.floor(Math.random() * 3);
     // Assign each random number a hand sign
     if (num === 0) {
+        // Rock option
         let play = "rock";
         return play;
     } else if (num === 1) {
+        // Paper option
         let play = "paper";
         return play;
     } else {
+        // Scissors option
         let play = "scissors";
         return play;
     }
+}
+
+// Function to determine and store the players input
+function playerSelection() {
+    // Ask for an input and store the string in a variable
+    let string = prompt("Please enter rock, paper, or scissors.");
+    
+    // Convert String to all lower case
+    let selection = string.toLowerCase();
+    return selection;
 }
 
